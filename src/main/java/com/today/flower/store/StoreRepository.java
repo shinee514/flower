c1e7c56c39ffb63f9675cd48d67532366a43ff8e
shinee514 <shinee10249@gmail.com>
2023-03-07 13:57:49 +0900

    7일

--- src/main/java/com/today/flower/store/StoreRepository.java
+++ src/main/java/com/today/flower/store/StoreRepository.java
@@ -1,7 +1,11 @@
 package com.today.flower.store;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoreRepository extends JpaRepository<Store, Integer> {
+	
+	List<Store> findByStoreId(Integer storeId);
 
 }
